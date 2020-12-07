7ea7d9e24c3a61e22755dbf6d0f5145496d75e55
Luke Schlangen <lukeschlangen@gmail.com>
2020-12-06 20:12:36 -0600

    add schlangen-isms to categories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,14 @@ import Player from '../Player/Player';
 import './App.css';
 import Logout from '../Logout/Logout';
 import { firebaseAuth } from '../../fire';
+import geckoImage from '../../images/gecko.png';
 
 const App = () => {
   const [user, setUser] = useState(null);
   useEffect(() => {
     firebaseAuth.onAuthStateChanged(
       (user) => {
-        if(user) {
+        if (user) {
           setUser({
             email: user.email,
             name: user.displayName,
@@ -30,6 +31,9 @@ const App = () => {
         <Logout user={user} />
         <Player user={user} />
       </>) : <Login />}
+      <center style={{marginTop: '400px'}}>
+        <img src={geckoImage} alt="Gecko Logo" width="100px" />
+      </center>
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { loginWithGoogle } from '../../fire';
 import './Login.css';
+import geckoImage from '../../images/gecko.png';
 
 const Login = () => (
-  <div className="container">
-    <button
-      className="primary-button login-button"
-      onClick={loginWithGoogle}
-    >
-    Login
+  <>
+    <div className="container">
+      <button
+        className="primary-button login-button"
+        onClick={loginWithGoogle}
+      >
+        Login
     </button>
-  </div>
+    </div>
+  </>
 );
 
 export default Login;
